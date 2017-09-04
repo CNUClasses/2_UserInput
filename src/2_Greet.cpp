3073d786cd0972470c1eb99e13adbe8f2553f290
Keith <kperkins411@gmail.com>
2017-09-03 23:31:52 -0400

    initial commit

---
+++ src/2_Greet.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : 2_Greet.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : ask for a person's name, and greet the person
+//============================================================================
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	// ask for the person's name
+	std::cout << "Please enter your first name: "<<std::endl;;
+
+	// read the name
+	std::string name;     // define `name'
+	std::cin >> name;     // read into `name'
+
+	// write a greeting
+	std::cout << "Hello, " << name  << "!" << std::endl;
+	return 0;
+}
